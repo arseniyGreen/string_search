3bb388043418ef15a5c5fb7fa33de30e69306717
Arseniy Arkadskov <43907861+arseniyGreen@users.noreply.github.com>
2021-11-16 12:47:58 +0300

    Upd

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "md5.h"
 
+#define FILEPATH "/home/notahacker/Documents/string_search/text.txt"
+
 int main() {
-    std::string toHash("Hi, this is a sample string.");
-    std::cout << "Sample string : " << toHash << "\nHashed: ";
-    std::cout << md5(toHash) << "\n";
+    std::ifstream file;
+    std::string text;
+    file.open(FILEPATH, std::ios::in);
+    if(file.is_open()){
+        std::string line;
+        while(std::getline(file, line)){
+            std::cout << line << std::endl;
+        }
+        file.close();
+    }
+    else{
+        std::cerr << "Unable to open file.\n";
+    }
     return 0;
 }
